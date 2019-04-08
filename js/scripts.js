7aae0e11ceb0d002713e3548bf326263aca9db6b
kwayne <wkudoi@gmail.com>
2019-04-08 11:55:47 +0300

    add code to declare declaration of winner

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,20 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     // Refresh after hold
     document.querySelector("#score-" + activePlayer).textContent =
       scores[activePlayer];
+
+      // Declaration of the winner
+    if (scores[activePlayer] >= 100) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
+  }
+});
