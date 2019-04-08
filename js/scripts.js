b3b884f44aae3a7e03cfbe26b4eb8ab244a31a4f
kwayne <wkudoi@gmail.com>
2019-04-08 11:47:36 +0300

    add randomization code and event listener

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 var scores, roundScore, activePlayer, gamePlaying, player1, player2;
 
 init();
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  if (gamePlaying) {
+    // Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+  
