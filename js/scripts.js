44d51ac78d7de6aae21475634135d1cf211de2cb
kwayne <wkudoi@gmail.com>
2019-04-08 11:48:21 +0300

    add rounding code to integer

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,6 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     // Random number
     var dice = Math.floor(Math.random() * 6) + 1;
 
-  
+    // result rounded to integer
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
